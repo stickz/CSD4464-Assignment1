c12c72edca0dc8b2e1687a4e8739436403686ce6
c0681133 <c0681133@SBA210630.lambton.on.ca>
2019-01-17 17:44:36 -0500

    Create random test case for stringPower method.

--- test/csd4464/assignment1/brandonbyronbrock/CSD4464Assignment1BrandonByronBrockTest.java
+++ test/csd4464/assignment1/brandonbyronbrock/CSD4464Assignment1BrandonByronBrockTest.java
@@ -5,6 +5,7 @@
  */
 package csd4464.assignment1.brandonbyronbrock;
 
+import java.util.Random;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -92,5 +93,30 @@ public class CSD4464Assignment1BrandonByronBrockTest {
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
         //fail("The test case is a prototype.");
+    } 
+    
+    @Test
+    public void testRandomStringconcatenation() {
+        System.out.println("\nRandom Test Case");        
+        String s = "apple";
+        
+        // Get a random number between 1 and 15     
+        int rNum = new Random().nextInt(15) + 1;
+        
+        // Generate the expected result for the random case
+        String expResult = "";
+        for (int i = 1; i <= rNum; i++) {
+            expResult += s;            
+        }
+        
+        // Find the actual result from the string power method
+        String result = CSD4464Assignment1BrandonByronBrock.stringPower(s, rNum);
+        
+        // Print the expected and found results
+        System.out.println("Found: " + result);
+        System.out.println("Expected: " + expResult + "\n");
+        
+        // Return wether the expected result was found
+        assertEquals(expResult, result);
     } 
 }
